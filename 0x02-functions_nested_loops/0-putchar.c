ae8c463e199076f13ca8022fb687d4dd3e269db5
WinnieNyangweso <winnienyangweso74@gmail.com>
2023-07-19 12:48:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,16 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-  * main - Entry point
-  *
-  * Return:0 (code is a a success)
-  */
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+ 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+ 
+printf("_putchar\n");
+ 
+return (0);
 }
-
